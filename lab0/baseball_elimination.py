25c75711624cfc65737fb3a8bf8f7d38005fb177
vickymmcd <vickymmcd@gmail.com>
2020-02-15 11:44:14 -0500

    adding some comments and starting to finalize things

--- lab0/baseball_elimination.py
+++ lab0/baseball_elimination.py
@@ -1,4 +1,7 @@
-# translated to python from https://github.com/ananya77041/baseball-elimination/blob/master/src/BaseballElimination.java
+'''Code file for baseball elimination lab created for Advanced Algorithms
+Spring 2020 at Olin College. The code for this lab has been adapted from:
+https://github.com/ananya77041/baseball-elimination/blob/master/src/BaseballElimination.java'''
+
 import sys
 import math
 import picos as pic
@@ -8,13 +11,26 @@ import swiglpk
 import cvxopt
 import re
 
+
 class Division:
+    '''
+    The Division class represents a baseball division. This includes all the
+    teams that are a part of that division, their winning and losing history,
+    and their remaining games for the season.
+
+    filename: name of a file with an input matrix that has info on teams &
+    their games
+    '''
+
     def __init__(self, filename):
         self.teams = {}
         self.G = nx.DiGraph()
         self.readDivision(filename)
 
     def readDivision(self, filename):
+        '''Reads the information from the given file and builds up a dictionary
+        of the teams that are a part of this division.
+        '''
         f = open(filename, "r")
         lines = [line.split() for line in f.readlines()]
         f.close()
@@ -25,9 +41,17 @@ class Division:
             self.teams[ID] = team
 
     def get_team_IDs(self):
+        '''Gets the list of IDs that are associated with each of the teams
+        in this division.
+        '''
         return self.teams.keys()
 
     def is_eliminated(self, teamID, solver):
+        '''Uses the given solver (either Linear Programming or Network Flows)
+        to determine if the team with the given ID is mathematically
+        eliminated from winning the division (aka winning more games than any
+        other team) this season.
+        '''
         flag1 = False
         flag2 = False
         team = self.teams[teamID]
@@ -36,18 +60,23 @@ class Division:
         del temp[teamID]
 
         for _, other_team in temp.items():
-            if team.get_wins() + team.get_remaining() < other_team.get_wins():
+            if team.wins + team.remaining < other_team.wins:
                 flag1 = True
 
+        saturated_edges = self.create_network(teamID)
         if solver == "Network Flows":
-            flag2 = self.network_flows(teamID)
+            flag2 = self.network_flows(teamID, saturated_edges)
         elif solver == "Linear Programming":
-            flag2 = self.linear_programming(teamID)
+            flag2 = self.linear_programming(teamID, saturated_edges)
 
         return flag1 or flag2
 
-    def network_flows(self, teamID):
-        saturated_edges = self.create_network(teamID)
+    def network_flows(self, teamID, saturated_edges):
+        '''Uses network flows to determine if the team with given team ID
+        has been eliminated. You can feel free to use the built in networkx
+        maximum flow function or the maximum flow function you implemented as
+        part of the in class implementation activity.
+        '''
         flow_value, flow_dict = nx.maximum_flow(self.G, 'source', 'sink')
 
         flag = False
@@ -58,6 +87,11 @@ class Division:
         return flag
 
     def create_network(self, teamID):
+        '''Builds up the network needed for solving the baseball elimination
+        problem as a network flows problem & stores it in self.G. Returns a
+        dictionary of saturated edges that maps team pairs to the amount of
+        additional games they have against each other.
+        '''
         self.G.clear()
 
         # helper dictionaries to hold capacities
@@ -73,9 +107,9 @@ class Division:
             matches[key] = temp[team1].get_against(team2)
             saturated_edges[key] = matches[key]
         # construct all the team max capacities
-        mainteam_max = self.teams[teamID].get_wins() + self.teams[teamID].get_remaining()
+        mainteam_max = self.teams[teamID].wins + self.teams[teamID].remaining
         for team in temp.keys():
-            teammaxes[f'{team}'] = mainteam_max - self.teams[team].get_wins()
+            teammaxes[f'{team}'] = mainteam_max - self.teams[team].wins
 
         # construct the actual graph
         # source to match edges
@@ -91,8 +125,11 @@ class Division:
 
         return saturated_edges
 
-    def linear_programming(self, teamID):
-        saturated_edges = self.create_network(teamID)
+    def linear_programming(self, teamID, saturated_edges):
+        '''Uses linear programming to determine if the team with given team ID
+        has been eliminated. We recommend using a picos solver to solve the
+        linear programming problem once you have it set up.
+        '''
 
         maxflow=pic.Problem()
 
@@ -105,6 +142,17 @@ class Division:
                 'f[{0}]'.format(edge),1,upper =self.G[edge[0]][edge[1]]['capacity'])
             if edge[0] is 'source':
                 source_edges.append(edge)
+
+        # # adding variables, contraints, and objective
+        # F = maxflow.add_variable('F', 1)
+        # for i in self.G.nodes():
+        #     if i == 'source':
+        #         maxflow.add_constraint(pic.sum([f[p,i] for p in self.G.predecessors(i)]) +
+        #                        F == pic.sum([f[i,p] for p in self.G.successors(i)]))
+        #     elif i != 'sink':
+        #         maxflow.add_constraint(pic.sum([f[p,i] for p in self.G.predecessors(i)])
+        #                                == pic.sum([f[i,p] for p in self.G.successors(i)]))
+        # maxflow.set_objective('max', F)
 
         # adding variables, contraints, and objective
         F = maxflow.add_variable('F', 1)
@@ -126,16 +174,35 @@ class Division:
 
 
     def checkTeam(self, team):
-        if team.get_ID() not in self.get_team_IDs():
+        '''Checks that the team actually exists in this division.
+        '''
+        if team.ID not in self.get_team_IDs():
             raise ValueError("Team does not exist in given input.")
 
     def __str__(self):
+        '''Returns pretty string representation of a division object.
+        '''
         temp = ''
         for key in self.teams:
             temp = temp + f'{key}: {str(self.teams[key])} \n'
         return temp
 
 class Team:
+    '''
+    The Team class represents one team within a baseball division for use in
+    solving the baseball elimination problem. This class includes information
+    on how many games the team has won and lost so far this season as well as
+    information on what games they have left for the season.
+
+    ID: ID to keep track of the given team
+    teamname: human readable name associated with the team
+    wins: number of games they have won so far
+    losses: number of games they have lost so far
+    remaining: number of games they have left this season
+    against: dictionary that can tell us how many games they have left against
+    each of the other teams
+    '''
+
     def __init__(self, ID, teamname, wins, losses, remaining, against):
         self.ID = ID
         self.name = teamname
@@ -144,19 +211,10 @@ class Team:
         self.remaining = remaining
         self.against = against
 
-    def get_ID(self):
-        return self.ID
-
-    def get_wins(self):
-        return self.wins
-
-    def get_losses(self):
-        return self.losses
-
-    def get_remaining(self):
-        return self.remaining
-
     def get_against(self, other_team=None):
+        '''Returns number of games this team has against this other team.
+        Raises an error if these teams don't play each other.
+        '''
         try:
             num_games = self.against[other_team]
         except:
@@ -165,10 +223,12 @@ class Team:
         return num_games
 
     def __str__(self):
+        '''Returns pretty string representation of a team object.
+        '''
         return f'{self.name} \t {self.wins} wins \t {self.losses} losses \t {self.remaining} remaining'
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     division = Division(filename)
     for (ID, team) in division.teams.items():
-        print(f'{team.name}: Eliminated? {division.is_eliminated(team.get_ID(), "Linear Programming")}')
+        print(f'{team.name}: Eliminated? {division.is_eliminated(team.ID, "Linear Programming")}')
--- lab0/test_baseball_elimination.py
+++ lab0/test_baseball_elimination.py
@@ -1,26 +1,40 @@
+'''Test file for baseball elimination lab created for Advanced Algorithms
+Spring 2020 at Olin College.'''
+
 from baseball_elimination import Division
 
+
 def assert_eliminated(division, team):
+    '''Asserts that the team in the given division is eliminated using both
+    the linear programming and the network flows methodology for solving.
+    Prints a failure message if they are not.
+    '''
     try:
-        assert division.is_eliminated(team.get_ID(), "Linear Programming") == True
+        assert division.is_eliminated(team.ID, "Linear Programming") == True
     except AssertionError:
         print("Failure in linear programming is eliminated: " + team.name + " should be eliminated.")
     try:
-        assert division.is_eliminated(team.get_ID(), "Network Flows") == True
+        assert division.is_eliminated(team.ID, "Network Flows") == True
     except AssertionError:
         print("Failure in network flows is eliminated: " + team.name + " should be eliminated.")
 
 def assert_not_eliminated(division, team):
+    '''Asserts that the team in the given division is not eliminated using both
+    the linear programming and the network flows methodology for solving.
+    Prints a failure message if the program returns that they are.
+    '''
     try:
-        assert division.is_eliminated(team.get_ID(), "Linear Programming") == False
+        assert division.is_eliminated(team.ID, "Linear Programming") == False
     except AssertionError:
         print("Failure in linear programming is eliminated: " + team.name + " should NOT be eliminated.")
     try:
-        assert division.is_eliminated(team.get_ID(), "Network Flows") == False
+        assert division.is_eliminated(team.ID, "Network Flows") == False
     except AssertionError:
         print("Failure in network flows is eliminated: " + team.name + " should NOT be eliminated.")
 
 def test_teams2():
+    '''Runs all test cases on the input matrix that can be found in teams2.txt.
+    '''
     division = Division("teams2.txt")
     for (ID, team) in division.teams.items():
         if team.name == "NewYork" or team.name == "Baltimore" or team.name == "Boston" or team.name == "Toronto":
@@ -29,6 +43,9 @@ def test_teams2():
             assert_eliminated(division, team)
 
 def test_teams4():
+    '''Runs all test cases on the input matrix that was given in the lab
+    description. It is stored in teams4.txt.
+    '''
     division = Division("teams4.txt")
     for (ID, team) in division.teams.items():
         if team.name == "Prava" or team.name == "Vicky":
