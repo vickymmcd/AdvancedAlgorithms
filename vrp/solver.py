5d9cbc87f305b655763cdbb82be3e5699b4a51dc
vickymmcd <vickymmcd@gmail.com>
2020-03-29 22:16:31 -0400

    mostly done with starter code, need to do test cases

---
+++ vrp/solver.py
@@ -0,0 +1,65 @@
+'''Code file for vehicle routing problem created for Advanced Algorithms
+Spring 2020 at Olin College. These functions solve the vehicle routing problem
+using an integer programming and then a local search approach. This code has
+been adapted from functions written by Alice Paul.'''
+
+import picos as pic
+import numpy as np
+from read_files import read_file_type_A, read_file_type_C
+
+
+def cvrp_ip(C,q,K,Q,obj=True):
+    '''
+    Solves the capacitated vehicle routing problem using an integer programming
+    approach.
+
+    C: matrix of edge costs, that represent distances between each node
+    q: list of demands associated with each client node
+    K: number of vehicles
+    Q: capacity of each vehicle
+    obj:
+    returns:
+        objective_value: value of the minimum travel cost
+        x: matrix representing number of routes that use each arc
+    '''
+    # TODO: add in destination node (same distances as source & demand = 0)
+
+    # set up the picos problem
+    prob = pic.Problem()
+
+    # TODO: add variables, constraints, and objective function!
+
+    x = []
+    objective_value = 0
+
+    return objective_value, x
+
+# Local search
+def local_search(C,q,K,Q):
+    '''
+    Solves the capacitated vehicle routing problem using a local search
+    approach.
+
+    C: matrix of edge costs, that represent distances between each node
+    q: list of demands associated with each client node
+    K: number of vehicles
+    Q: capacity of each vehicle
+    returns:
+        bestval: value of the minimum travel cost
+        bestx: matrix representing number of routes that use each arc
+    '''
+    bestx = []
+    bestval = 0
+
+    # TODO: implement local search to solve vehicle routing problem
+
+    return bestval, bestx
+
+
+if __name__ == "__main__":
+
+    C,q,K,Q = read_file_type_A('data2/A-n016-k05.xml')
+    #C,q,Q = read_instance_data3('data3/LD11_1.xml')
+
+    print(cvrp_ip(C,q,K,Q))
+    print(local_search(C,q,K,Q))
--- vrp/solver_soln.py
+++ vrp/solver_soln.py
@@ -1,3 +1,8 @@
+'''Code file for vehicle routing problem created for Advanced Algorithms
+Spring 2020 at Olin College. These functions solve the vehicle routing problem
+using an integer programming and then a local search approach. This code has
+been adapted from functions written by Alice Paul.'''
+
 import picos as pic
 import numpy as np
 from read_files import read_file_type_A, read_file_type_C
@@ -17,62 +22,94 @@ def DFS(i,x,visited,S):
 
     return S,visited[0]
 
-# IP
 def cvrp_ip(C,q,K,Q,obj=True):
+    '''
+    Solves the capacitated vehicle routing problem using an integer programming
+    approach.
+
+    C: matrix of edge costs, that represent distances between each node
+    q: list of demands associated with each client node
+    K: number of vehicles
+    Q: capacity of each vehicle
+    obj:
+    returns:
+        objective_value: value of the minimum travel cost
+        x: matrix representing number of routes that use each arc
+    '''
+    # need to add in destination node (same as source)
     n = len(q)+1
     Cnew = np.zeros((n,n))
     for i in range(n-1):
         for j in range(i+1,n-1):
             Cnew[i,j] = C[i,j]
             Cnew[j,i] = C[j,i]
+            # set destination node to have same distances as source
             if i == 0:
                 Cnew[n-1,j] = C[i,j]
                 Cnew[j,n-1] = C[j,i]
+    # destination node, like origin, has 0 demand
     qnew = np.append(q,[0])
-    print(C)
-    print(Cnew)
-    print(q)
-    print (qnew)
 
+    # set up the picos problem
     prob = pic.Problem()
+
     # Add variables
+
+    # x represents # of routes that use each arc
     x = prob.add_variable('x',(n,n),vtype="binary")
-    print(x)
+    # u represents cumulative quantity of goods delivered between origin and a given node
     u = prob.add_variable('u',n,vtype="continuous",upper=Q,lower=q)
 
     # Add flow conservation constraints
-    prob.add_constraint(sum([x[0,j] for j in range(n)]) <= K)
-    prob.add_constraint(sum([x[i,0] for i in range(n)]) == 0)
-    prob.add_constraint(sum([x[i,i] for i in range(n)]) == 0)
+    prob.add_constraint(sum([x[0,j] for j in range(n)]) <= K) # no more than K routes for K vehicles
+    prob.add_constraint(sum([x[i,0] for i in range(n)]) == 0) # routes can't end at the origin
+    prob.add_constraint(sum([x[i,i] for i in range(n)]) == 0) # routes can't include edges connecting node to itself
+    # number of routes that leave origin = number that enter the destination
     prob.add_constraint(sum([x[0,j] for j in range(n)]) == sum([x[i,n-1] for i in range(n)]))
+    # every node must be visited - an edge goes from another node to it
     prob.add_list_of_constraints([sum([x[i,j] for j in range(n)]) == 1 for i in range(1,n-1)])
+    # route must also leave that node - edge goes from it to another node
     prob.add_list_of_constraints([sum([x[j,i] for j in range(n)]) == 1 for i in range(1,n-1)])
 
     # Capacity constraints
     prob.add_list_of_constraints([u[i]-u[j]+Q*x[i,j] <= Q-qnew[j] for i in range(n) for j in range(n) ])
 
-    # Objective function
+    # Objective function - minimize travel cost
     prob.set_objective('min',pic.sum([Cnew[i,j]*x[i,j] for i in range(n) for j in range(n)]))
+
+    # ?? what is this about??
     if obj == False:
         prob.set_objective('min',x[0,0])
 
-    #print prob
-
     # Solve
     solution = prob.solve(solver="cplex",verbose=True,gaplim=1e-2)
-    print("solution is: " + str(solution))
+
     if ("optimal" not in solution['status']):
         return None
 
+    # print edges that are included in the solution
     for i in range(n):
        for j in range(n):
            if x[i,j].value == 1.0:
                print ("Edge "+str(i)+","+str(j))
 
-    return prob.obj_value(), x
+    objective_value = prob.obj_value()
+    return objective_value, x
 
 # Local search
 def local_search(C,q,K,Q):
+    '''
+    Solves the capacitated vehicle routing problem using a local search
+    approach.
+
+    C: matrix of edge costs, that represent distances between each node
+    q: list of demands associated with each client node
+    K: number of vehicles
+    Q: capacity of each vehicle
+    returns:
+        bestval: value of the minimum travel cost
+        bestx: matrix representing number of routes that use each arc
+    '''
     n = len(q)
     _, xinit = cvrp_ip(C,q,K,Q,False)
     max_iters = 1
@@ -130,5 +167,5 @@ if __name__ == "__main__":
     C,q,K,Q = read_file_type_A('data2/A-n016-k05.xml')
     #C,q,Q = read_instance_data3('data3/LD11_1.xml')
 
-    print(cvrp_ip(C,q,K,Q))
-    #print(local_search(C,q,K,Q))
+    #print(cvrp_ip(C,q,K,Q))
+    print(local_search(C,q,K,Q))
